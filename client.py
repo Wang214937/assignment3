752bc97eb94756365908c03e0861b5c5a6dcc394
Wang Jiarui <2766259133@qq.com>
2025-05-02 21:51:56 +0800

    open the file

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 import socket
-import threading
+import sys
+
+class Client:
+    def __init__(self, host, port,file_path):
+        try:
+            with open(file_path) as file:
+                self.requests = [line.strip() for line in file.readlines() if line.strip()]
+        except FileNotFoundError:
+            print(f"Error: File {file_path} not found.")
+            sys.exit(1)
+
+
+    
+
+       
