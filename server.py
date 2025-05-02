17b4eb495d1f1137ab685bc5b4500bd808fffd6c
Wang Jiarui <2766259133@qq.com>
2025-05-02 17:45:06 +0800

    create some variables

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
 import socket
 import threading
+
+class Server:
+    def __init__(self, port):
+        self.tuple = {}
+        self.lock = threading.Lock()
+        self.state = {
+            "reads":0,
+            "gets":0,
+            "puts":0,
+            "errs":0
+        }
